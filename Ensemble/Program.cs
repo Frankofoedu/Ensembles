1a158766bd463c735f0f6e3dab83d9557c4efd55
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-08-20 20:47:06 +0100

    Add project files.

---
+++ Ensemble/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms;
+using ML.Models;
+using System;
+using System.Linq;
+
+namespace Ensemble
+{
+    class Program
+    {
+
+        //Create MLContext
+        static readonly MLContext mlContext = new MLContext();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LoadData(mlContext);
+
+            Console.Read();
+        }
+
+        private static void LoadData(MLContext mLContext)
+        {
+
+            //Load Data
+            IDataView data = mLContext.Data.LoadFromTextFile<CreditCardData>("Data/creditcard.csv", separatorChar: ',', hasHeader: true);
+
+            //drop the id column 
+
+           var pipeline = mLContext.Transforms.DropColumns("Id");
+
+            
+            
+        }
+
+        NormalizingTransformer StandardizeData(IDataView data)
+        {
+            // Define min-max estimator
+            var minMaxEstimator = mlContext.Transforms.norma("Price");
+
+            // Fit data to estimator
+            // Fitting generates a transformer that applies the operations of defined by estimator
+            return minMaxEstimator.Fit(data);
+        }
+    }
+}
---
+++ ML.Models/Card.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Models
+{
+
+    public class CreditCardData
+    {
+        [LoadColumn(1)]
+        public float LIMIT_BAL;
+        [LoadColumn(2)]
+        public float SEX;
+        [LoadColumn(3)]
+        public float EDUCATION;
+        [LoadColumn(4)]
+        public float MARRIAGE;
+
+        [LoadColumn(5)]
+        public float AGE;
+
+        [LoadColumn(6, 12)]
+        [VectorType(7)]
+        public float[] Payments;
+
+        [LoadColumn(12,17)]
+        [VectorType(6)]
+        public float[] BILL_AMT1;
+
+        [LoadColumn(18,23)]
+        public float[] PAY_AMT1;
+
+        [LoadColumn(24), ColumnName("Label")]
+        public bool isDefault;
+    }
+
+    public class CreditCardPrediction : CreditCardData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        //public float Probability { get; set; }
+
+        //public float Score { get; set; }
+    }
+}
